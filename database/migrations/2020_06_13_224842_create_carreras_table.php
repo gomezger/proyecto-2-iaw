de92241664f8aa253fbe55c78ad098c514001fe2
Xeron081495 <germang08@hotmail.com>
2020-06-14 18:48:46 -0300

    agregar tablas migrate

---
+++ database/migrations/2020_06_13_224842_create_carreras_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carreras', function (Blueprint $table) {
+
+            $table->string('codigo');
+            $table->string('nombre');
+            $table->integer('duracion');
+
+            $table->primary('codigo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carreras');
+    }
+}
